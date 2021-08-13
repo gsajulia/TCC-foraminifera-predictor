593a42276caa553cf8a83641dcea531dfe59610a
Júlia Acosta <juliagabriela98@gmail.com>
2021-08-13 14:35:58 -0300

    change header layout to the prototype style

--- ui.R
+++ ui.R
@@ -1,50 +1,53 @@
 library(shinydashboard)
-#(shinydashboardPlus)
 
 my_theme <- create_theme(
   adminlte_color(
-      aqua = "#FFC0CB", #Test
       light_blue = "#28A390"
   ),
   adminlte_sidebar(
-    dark_bg = "#1F1F1F"
+    dark_bg = "#212936"
   )
 )
 
 
 # Put them together into a dashboardPage
 ui <- dashboardPage(
-      dashboardHeader(title = "Foraminifera"),
-      dashboardSidebar(
-        collapsed=TRUE,
-        sidebarMenu(
-          menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-          menuItem("Widgets", icon = icon("th"), tabName = "widgets",
-                   badgeLabel = "new", badgeColor = "red")
-        )
-      ),
-      dashboardBody(
-        use_theme(my_theme),
-        # Including css
-        tags$link(
-          rel =  "stylesheet",
-          type = "text/css",
-          href = "styles.css"
+      dashboardHeader(title = "Foraminiferal"),
+        dashboardSidebar(
+          collapsed=FALSE,
+          sidebarMenu(
+            tags$div(`class` = "subtitle", "Predictions"),
+            hr(),
+            menuItem("Predict", tabName = "predict", icon = icon("searchengin")),
+            menuItem("Neural Network Info", icon = icon("network-wired"), tabName = "neuralNetwork"),
+            menuItem("Graphs", icon = icon("chart-bar"), tabName = "graphs"),
+            hr(),
+            menuItem("Help", icon = icon("question"), tabName = "help")
+          )
         ),
-        # Including fonts
-        tags$link(
-          rel =  "stylesheet",
-          href="https://fonts.googleapis.com/css2?family=Qahiri&display=swap"
-        ),
-        tabItems(
-          tabItem(tabName = "dashboard",
-                  graph_UI("graph_test")
-                  
+        dashboardBody(
+          use_theme(my_theme),
+          tags$style("@import url(https://use.fontawesome.com/releases/v5.7.2/css/all.css);"),
+          # Including css
+          tags$link(
+            rel =  "stylesheet",
+            type = "text/css",
+            href = "styles.css"
           ),
-          
-          tabItem(tabName = "widgets",
-                  h2("Widgets tab content")
+          # Including fonts
+          tags$link(
+            rel =  "stylesheet",
+            href="https://fonts.googleapis.com/css2?family=Qahiri&display=swap"
+          ),
+          tabItems(
+            tabItem(tabName = "dashboard",
+                    graph_UI("graph_test")
+                    
+            ),
+            
+            tabItem(tabName = "widgets",
+                    h2("Widgets tab content")
+            )
           )
         )
-      )
     )
